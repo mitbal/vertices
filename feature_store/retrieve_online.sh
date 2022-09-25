f280038fc4d34219098998a2838ece4f28585b2f
M Iqbal Tawakal <mit.iqi@gmail.com>
2022-09-26 00:27:34 +0700

    Removed duplicate script

--- feature_store/retrieve_online.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST \
--H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
--H "Content-Type: application/json; charset=utf-8" \
--d @request1.json \
-"https://asia-southeast1-aiplatform.googleapis.com/v1/projects/mitochondrion-project-344303/locations/asia-southeast1/featurestores/marketing_featurestore/entityTypes/users:readFeatureValues"
-
